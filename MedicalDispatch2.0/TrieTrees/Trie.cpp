b195bdaff988fc86823b4f17f7eb2e3de5ef16c8
bezjac <mybj20@gmail.com>
2021-11-26 03:27:58 +0200

    added comments

--- MedicalDispatch2.0/TrieTrees/Trie.cpp
+++ MedicalDispatch2.0/TrieTrees/Trie.cpp
@@ -5,23 +5,27 @@ Trie::Trie()
 }
 void Trie::addWord(string key)
 {
+	// check if word already exsists in tree
 	if (searchWord(key))
 	{
 		cout << "word exists, no need to insert\n";
 		return;
 	}
 	TrieNode* node = root;
+	// create path of word in tree, if node in path is NULL, initialize th node
 	for (int i = 0; i < key.length(); i++)
 	{
 		if (node->children[key[i] - 97] == NULL)
 			node->children[key[i] - 97] = new TrieNode(node);
 			node = node->children[key[i] - 97];
 	}
+	// set  of end of word node flag to true
 	node->isEndOfWord = true;
 }
 bool Trie::deleteWord(string key)
 {
 	TrieNode* node = root;
+	// find path of word in tree - node points to  "claimed" end of the  word node
 	for (int i = 0; i < key.length(); i++)
 	{
 		if (node->children[key[i] - 97] == NULL)
@@ -29,14 +33,20 @@ bool Trie::deleteWord(string key)
 		else
 			node = node->children[key[i] - 97];
 	}
+	// node is not an end of a word - word dosen't exsist in tree - return false
 	if (node->isEndOfWord==false)
 		return false;
+	// node is a valid end of word- check if node has children, if positive,
+	// set end of word flag of node to false and return true
 	if (hasChildren(node))
 	{
 		node->isEndOfWord = false;
 		return true;
 	}
-	TrieNode* temp = node->parent;
+	// node does not have children - node is deleted
+	// for each father in path back to root check if father has children or is a
+	// end of a word -if positive stop deletion - return true.
+	TrieNode* temp = node->parent;   // temp points to cuurent loop  node's father
 	for (int i = key.length()-1; i >= 0; i--)
 	{
 		if (i != key.length() - 1)
@@ -55,6 +65,8 @@ bool Trie::searchWord(string key,TrieNode* node)
 {
 	if (node == NULL)
 		return false;
+	// iterate through node's path, if any node in path is NULL
+	// word does not exsist- return false
 	for (int i = 0; i < key.length(); i++)
 	{
 		if (node->children[key[i] - 97] == NULL)
@@ -62,33 +74,35 @@ bool Trie::searchWord(string key,TrieNode* node)
 		else
 			node = node->children[key[i] - 97];
 	}
+	// end of word reached, check if current node is actually end of a word
 	if (node->isEndOfWord)
 		return true;
+	// word does not exsist in tree
 	return false;
 }
 int Trie::printAutoSuggestions(string key)
 {
 	TrieNode* node = root;
-	string pointer;
+	char wordsArray[ALPHABET_SIZE * 100];
+	// find node that contains last charachter of string
+	// add each charachter in loop to array sent to recursive printing function, to enable prefix
+	// printing for each suggestion
 	for (int i = 0; i < key.length(); i++)
 	{
 		if (node->children[key[i] - 97] == NULL)
 			return false;
 		else
 		{
-			pointer += key[i];
+			wordsArray[i] = key[i];
 			node = node->children[key[i] - 97];
 		}
 	}
-	char wordsArray[ALPHABET_SIZE*100];
-	for (int i = 0; i < pointer.length(); i++)
-	{
-		wordsArray[i] = pointer[i];
-	}
-	return printAllWordFromPrefix( node, wordsArray,pointer.length());
+	// send found node and  array containing prefix to print.
+	return printAllWordFromPrefix( node, wordsArray,key.length());
 }
 int  Trie::printAllWordFromPrefix(TrieNode* node, char* wordsArray, int position, int count)
 {
+	//  end of word reached end recursion call - print the word
 	if (node->isEndOfWord)
 	{
 		count++;
@@ -99,20 +113,19 @@ int  Trie::printAllWordFromPrefix(TrieNode* node, char* wordsArray, int position
 		cout << endl;
 		
 	}
+	// iterative recursive call to run through a given node to check all 
+	// exsisting children for print
 	for (int i = 0; i < ALPHABET_SIZE; i++)
 	{
+		// node has child - recursive call to iterate through subtree rooted in current child
 		if (node->children[i] != NULL)
 		{
-			
-			
 			wordsArray[position] = i + 'a';
 			count += printAllWordFromPrefix(node->children[i], wordsArray, position + 1);
 		}
 	}
 	return count;
 }
-	
-
 bool Trie::hasChildren(TrieNode* node)
 {
 	for (int i = 0; i < ALPHABET_SIZE; i++)
--- MedicalDispatch2.0/TrieTrees/Trie.h
+++ MedicalDispatch2.0/TrieTrees/Trie.h
@@ -22,13 +22,54 @@ public:
     TrieNode* root;
 public:
     Trie();
+    /// <summary>
+/// add a new word to Trie tree
+/// </summary>
+/// <param name="key"> : the word to add</param>
     void addWord(string);
+    /// <summary>
+/// delete a word from Trie tree
+/// </summary>
+/// <param name="key"> : the word to delete </param>
+/// <returns> true if delition was successful, othewise false</returns>
     bool deleteWord(string);
+    /// <summary>
+    /// check if word is in tree,
+    /// public method ,
+    ///  calls private method with tree root 
+    /// </summary>
+    /// <param name="key"> word to serch for </param>
+    /// <returns> true if word was found, otherwise false</returns>
     bool searchWord(string key) { return searchWord(key, root); }
+    /// <summary>
+/// print Auto suggestions for word endings to charachters enterd by user,
+/// from the words in the tree
+/// </summary>
+/// <param name="key"> string of input charachters from user </param>
+/// <returns></returns>
     int printAutoSuggestions(string key);
 private:
+    /// <summary>
+/// search for a word in a Trie tree
+/// </summary>
+/// <param name="key"> : word to search</param>
+/// <param name="node"> : tree's root </param>
+/// <returns> true if found, otherwise false</returns>
     bool searchWord(string, TrieNode*);
+    /// <summary>
+/// check if a given node in Trie tree has children
+/// </summary>
+/// <param name="node"> - node in check</param>
+/// <returns> true if node has children, otherwise false</returns>
     bool hasChildren(TrieNode*);
+    /// <summary>
+/// print all words in Trie tree rooted in a given node
+/// </summary>
+/// <param name="node">  - node  serving as root to print from </param>
+/// <param name="wordsArray"> -  array to copy words to </param>
+/// <param name="position">  - current position in array</param>
+/// <param name="count"> -  number of words printed</param>
+/// <returns> number of words successfuly printed </returns>
     int printAllWordFromPrefix(TrieNode*, char* wordsArray,int position, int count =0 );
 
 };
